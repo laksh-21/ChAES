0c5d67337c9d701d0e2984bc0f8e8d172ef66c69
laksh-21 <iamlakshable@gmail.com>
2021-06-17 14:15:55 +0530

    Add BottomNavBar

--- app/src/main/java/com/example/chaes/AppScaffold.kt
+++ app/src/main/java/com/example/chaes/AppScaffold.kt
@@ -1,16 +1,22 @@
 package com.example.chaes
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun AppScaffold(
     navController: NavController
 ){
     val scope = rememberCoroutineScope()
-
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
     val bottomBar: @Composable () -> Unit = {
-
+        if(navBackStackEntry?.destination?.route == Screens.BottomNavScreens.Home.route ||
+            navBackStackEntry?.destination?.route == Screens.BottomNavScreens.Search.route){
+            BottomNavBar(navController = navController, bottomNavScreens)
+        }
     }
 }
