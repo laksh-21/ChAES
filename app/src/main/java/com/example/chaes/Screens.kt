d53d72580083bc9ebccf0141375486e27fd7b4d7
laksh-21 <iamlakshable@gmail.com>
2021-06-17 13:40:15 +0530

    Add bottom screens

---
+++ app/src/main/java/com/example/chaes/Screens.kt
@@ -0,0 +1,22 @@
+package com.example.chaes
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screens(val route: String, val title: String){
+    sealed class BottomNavScreens(
+        route: String,
+        title: String,
+        val icon: ImageVector
+    ): Screens(route, title){
+        object Home: BottomNavScreens("homeScreen", "Home", Icons.Filled.Home)
+        object Search: BottomNavScreens("searchScreen", "Search", Icons.Filled.Search)
+    }
+}
+
+val bottomNavScreens = listOf(
+    Screens.BottomNavScreens.Home,
+    Screens.BottomNavScreens.Search
+)
