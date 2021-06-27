be82c4e3ac2b88043a198a80be55ef6093156195
laksh-21 <iamlakshable@gmail.com>
2021-06-27 20:52:50 +0530

    Add basic Login Screen

--- app/src/main/java/com/example/chaes/login/LoginScreen.kt
+++ app/src/main/java/com/example/chaes/login/LoginScreen.kt
@@ -1,24 +1,49 @@
 package com.example.chaes.login
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.chaes.bottomNavScreensRoute
 
 @Composable
 fun LoginScreen(
-    navController: NavController
+    navController: NavController?
 ){
-    Button(onClick = {
-        navController.navigate(bottomNavScreensRoute){
-            navController.graph.startDestinationRoute?.let{
-                popUpTo(it){
-                    inclusive = true
-                }
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.h3,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Please sign-in to continue",
+            style = MaterialTheme.typography.subtitle2,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(50),
+            contentPadding = PaddingValues(horizontal = 40.dp, vertical = 16.dp),
+        ){
+            Text(text = "Login")
         }
-    }){
-        Text(text = "Click me!")
     }
+}
+
+@Preview
+@Composable
+fun LoginScreenDemo(){
+    LoginScreen(navController = null)
 }
