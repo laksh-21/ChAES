6c50f34ce8b0279a65a9cec1d8569c4106628c1d
laksh-21 <iamlakshable@gmail.com>
2021-06-28 16:46:02 +0530

    Finish Login Screen Design

--- app/src/main/java/com/example/chaes/login/LoginScreen.kt
+++ app/src/main/java/com/example/chaes/login/LoginScreen.kt
@@ -5,12 +5,17 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.chaes.login.components.InfoTextField
 
 @Composable
 fun LoginScreen(
@@ -25,19 +30,60 @@ fun LoginScreen(
         Text(
             text = "Login",
             style = MaterialTheme.typography.h3,
+            modifier = Modifier.fillMaxWidth(.9f),
+            textAlign = TextAlign.Start
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
-            text = "Please sign-in to continue",
+            text = "Please sign-in to continue.",
             style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.fillMaxWidth(.9f),
+            textAlign = TextAlign.Start
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        InfoTextField(
+            hintText = "EMAIL",
+            leadingIcon = Icons.Outlined.Email
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        InfoTextField(
+            hintText = "PASSWORD",
+            leadingIcon = Icons.Outlined.Lock
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {},
             shape = RoundedCornerShape(50),
-            contentPadding = PaddingValues(horizontal = 40.dp, vertical = 16.dp),
+            modifier = Modifier.fillMaxWidth(0.5f)
         ){
-            Text(text = "Login")
+            Text(
+                text = "LOGIN",
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Forgot Password?",
+            style = MaterialTheme.typography.subtitle2,
+            color = MaterialTheme.colors.primary,
+        )
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+                .padding(bottom = 16.dp)
+        ){
+            Text(
+                text = "Not Registered Yet?",
+                style = MaterialTheme.typography.subtitle2,
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "Sign Up",
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.primary,
+            )
         }
     }
 }
--- app/src/main/java/com/example/chaes/login/components/InfoTextField.kt
+++ app/src/main/java/com/example/chaes/login/components/InfoTextField.kt
@@ -1,26 +1,35 @@
 package com.example.chaes.login.components
 
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun InfoTextField(
     hintText: String = "Hint",
-    leadingIcon: ImageVector
+    leadingIcon: ImageVector,
 ){
     TextField(
+        modifier = Modifier.fillMaxWidth(),
         value = "",
         label = { Text(text = hintText) },
         onValueChange = {},
-        leadingIcon = { Icon(imageVector = leadingIcon, contentDescription = hintText) }
+        leadingIcon = { Icon(imageVector = leadingIcon, contentDescription = hintText) },
+        shape = RoundedCornerShape(25),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        maxLines = 1,
+        singleLine = true,
     )
 }
 
