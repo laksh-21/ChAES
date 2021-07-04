455da089b2a71b06fb3ed382680f3af92dcd02e0
laksh-21 <iamlakshable@gmail.com>
2021-07-04 19:00:27 +0530

    Design Sign-up screen

--- app/src/main/java/com/example/chaes/login/SignUpScreen.kt
+++ app/src/main/java/com/example/chaes/login/SignUpScreen.kt
@@ -1,12 +1,88 @@
 package com.example.chaes.login
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.chaes.login.components.HeaderSection
+import com.example.chaes.login.components.InfoTextField
 
 @Composable
 fun SignUpScreen(
-    navController: NavController
+    navController: NavController?
 ){
-    Text(text = "Hello Signup")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        HeaderSection(
+            mainText = "Create Account",
+            subText = "Please fill the inputs below."
+        )
+        InfoFieldSection()
+    }
+}
+
+@Composable
+private fun InfoFieldSection(){
+    InfoTextField(
+        hintText = "Full Name",
+        leadingIcon = Icons.Outlined.Person,
+        text = "",
+        onTextChanged = {},
+        keyboardType = KeyboardType.Text
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    InfoTextField(
+        hintText = "Phone",
+        leadingIcon = Icons.Outlined.Phone,
+        text = "",
+        onTextChanged = {},
+        keyboardType = KeyboardType.Phone
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    InfoTextField(
+        hintText = "Email",
+        leadingIcon = Icons.Outlined.Email,
+        text = "",
+        onTextChanged = {},
+        keyboardType = KeyboardType.Email
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    InfoTextField(
+        hintText = "Password",
+        leadingIcon = Icons.Outlined.Lock,
+        text = "",
+        onTextChanged = {},
+        keyboardType = KeyboardType.Password,
+        obfuscate = true
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    InfoTextField(
+        hintText = "Confirm Password",
+        leadingIcon = Icons.Outlined.Lock,
+        text = "",
+        onTextChanged = {},
+        keyboardType = KeyboardType.Password,
+        obfuscate = true
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+}
+
+@Preview
+@Composable
+fun SignupScreenDemo(){
+    SignUpScreen(navController = null)
+    InfoFieldSection()
 }
