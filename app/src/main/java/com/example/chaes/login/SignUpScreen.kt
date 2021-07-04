6180ae2ef851fc3bcc1a5ffafadf1939c36167ec
laksh-21 <iamlakshable@gmail.com>
2021-07-05 02:14:14 +0530

    Finish Sign up design

--- app/src/main/java/com/example/chaes/login/SignUpScreen.kt
+++ app/src/main/java/com/example/chaes/login/SignUpScreen.kt
@@ -1,6 +1,11 @@
 package com.example.chaes.login
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Lock
@@ -10,9 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.chaes.Screens
 import com.example.chaes.login.components.HeaderSection
 import com.example.chaes.login.components.InfoTextField
 
@@ -31,6 +38,47 @@ fun SignUpScreen(
             subText = "Please fill the inputs below."
         )
         InfoFieldSection()
+        Spacer(modifier = Modifier.height(16.dp))
+        LoginButton()
+        LoginRow()
+    }
+}
+
+@Composable
+fun LoginRow(){
+    Row(
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 16.dp)
+    ){
+        Text(
+            text = "Already hve an account?",
+            style = MaterialTheme.typography.subtitle2,
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.subtitle2,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier.clickable(onClick = { })
+        )
+    }
+}
+
+@Composable
+fun LoginButton(){
+    Button(
+        onClick = {},
+        shape = RoundedCornerShape(50),
+        modifier = Modifier.fillMaxWidth(0.5f)
+    ) {
+        Text(
+            text = "SIGN UP",
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
