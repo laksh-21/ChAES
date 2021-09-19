4f9c9a8bb37f6e91f8376eb48120c58817085ad7
laksh21 <iamlakshable@gmail.com>
2021-09-19 09:56:58 +0530

    Update encryptor to accept Message.kt

--- app/src/main/java/com/example/chaes/models/Message.kt
+++ app/src/main/java/com/example/chaes/models/Message.kt
@@ -5,8 +5,10 @@ import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
 data class Message(
-    val content: String = "Hello!",
-    val senderName: String = Constants.fromUserMessageName,
+    val content: String? = "Hello!",
+    val senderName: String? = Constants.fromUserMessageName,
+    val messageIv: String? = "hahaIV",
+    val messageSalt: String? = "hahaSalt",
     @ServerTimestamp
     val messageTime: Date? = null,
 )
--- app/src/main/java/com/example/chaes/utilities/Encryptor.kt
+++ app/src/main/java/com/example/chaes/utilities/Encryptor.kt
@@ -1,7 +1,7 @@
 package com.example.chaes.utilities
 
 import android.os.Build
-import androidx.annotation.RequiresApi
+import com.example.chaes.models.Message
 import timber.log.Timber
 import java.security.SecureRandom
 import java.util.*
@@ -13,9 +13,43 @@ import javax.crypto.spec.SecretKeySpec
 
 class Encryptor {
     private val password = "@ppl3_p!3_hue"
-    private var hashMap = mapOf<String, String>()
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun encrypt(message: String){
+    private val ivKey = "iv"
+    private val saltKey = "salt"
+    private val messageKey = "encrypted"
+
+    fun encryptMessage(
+        message: String,
+        senderName: String
+    ): Message{
+        val messageMap = encrypt(message = message)
+        return Message(
+            content = messageMap[messageKey],
+            senderName = senderName,
+            messageIv = messageMap[ivKey],
+            messageSalt = messageMap[saltKey]
+        )
+    }
+
+    fun decryptMessage(encryptedMessage: Message): Message {
+        if(encryptedMessage.messageSalt == null ||
+                encryptedMessage.messageIv == null ||
+                encryptedMessage.content == null){
+            return encryptedMessage
+        }
+        val decryptedMessage = decrypt(
+            messageSalt = encryptedMessage.messageSalt,
+            messageIv = encryptedMessage.messageIv,
+            encryptedMessage = encryptedMessage.content
+        )
+        return Message(
+            content = decryptedMessage,
+            senderName = encryptedMessage.senderName,
+            messageIv = encryptedMessage.messageIv,
+            messageSalt = encryptedMessage.messageSalt
+        )
+    }
+
+    private fun encrypt(message: String): Map<String, String>{
         // generating salt
         val saltRandom = SecureRandom()
         val salt = ByteArray(256)
@@ -36,23 +70,43 @@ class Encryptor {
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec)
         val encryptedMessage = cipher.doFinal(message.toByteArray())
 
-//        Timber.d("Key = %s", String(keyBytes))
-//        Timber.d("IV = %s", String(iv))
-//        Timber.d("Salt = %s", String(salt))
-//        Timber.d("Encrypted Message = %s", String(encryptedMessage))
+        val hashMap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mapOf(
+                "iv" to Base64.getEncoder().encodeToString(iv),
+                "salt" to Base64.getEncoder().encodeToString(salt),
+                "encrypted" to Base64.getEncoder().encodeToString(encryptedMessage)
+            )
+        } else{
+            mapOf(
+                "iv" to android.util.Base64.encodeToString(iv, android.util.Base64.DEFAULT),
+                "salt" to android.util.Base64.encodeToString(salt, android.util.Base64.DEFAULT),
+                "encrypted" to android.util.Base64.encodeToString(encryptedMessage, android.util.Base64.DEFAULT),
+            )
+        }
 
-        hashMap = mapOf(
-            "iv" to Base64.getEncoder().encodeToString(iv),
-            "salt" to Base64.getEncoder().encodeToString(salt),
-            "encrypted" to Base64.getEncoder().encodeToString(encryptedMessage)
-        )
+        return hashMap
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun decrypt(){
-        val salt = Base64.getDecoder().decode(hashMap["salt"]!!)
-        val iv = Base64.getDecoder().decode(hashMap["iv"]!!)
-        val encrypted = Base64.getDecoder().decode(hashMap["encrypted"]!!)
+    private fun decrypt(
+        messageIv: String,
+        messageSalt: String,
+        encryptedMessage: String
+    ):String{
+        val salt = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Base64.getDecoder().decode(messageSalt)
+        } else {
+            android.util.Base64.decode(messageSalt, android.util.Base64.DEFAULT)
+        }
+        val iv = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Base64.getDecoder().decode(messageIv)
+        } else {
+            android.util.Base64.decode(messageIv, android.util.Base64.DEFAULT)
+        }
+        val encrypted = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Base64.getDecoder().decode(encryptedMessage)
+        } else {
+            android.util.Base64.decode(encryptedMessage, android.util.Base64.DEFAULT)
+        }
 
         val pbKeySpec = PBEKeySpec(password.toCharArray(), salt, 1234, 256)
         val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
@@ -64,9 +118,7 @@ class Encryptor {
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
         val decrypted = cipher.doFinal(encrypted)
 
-//        Timber.d("Key = %s", String(keyBytes))
-//        Timber.d("IV = %s", String(iv!!))
-//        Timber.d("Salt = %s", String(salt!!))
         Timber.d("Decrypted Message = %s", String(decrypted))
+        return String(decrypted)
     }
 }
